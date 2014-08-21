70e422849983ac5d8b8b301afcdd88611707d19f
Rafael Minguet García <rminguet@gmail.com>
2014-08-21 16:06:54 +0200

    Mejoras en el código y organización.

--- README.md
+++ README.md
@@ -3,10 +3,16 @@ El objetivo de este código es crear un *slideshow* tipo carrusel que mostrará 
 
 Está escrito en jQuery, así que se necesitará la librería para hacer que funcione. Puede descargarse desde [aquí](http://jquery.com/download/).
 
-Además, es necesario que las imágenes estén en una lista `<li>` dentro de un elemento con id `#slideshow`; esto se puede adaptar fácilmente a las necesidades del programador editando simplemente la línea:
+Además, es necesario que las imágenes estén en una lista `<li>` dentro de un elemento con id `#slideshow` tal y como se puede leer en el código:
 
 ```javascript
-var carousel = $('#slideshow>li');
+var $elements = $('#slideshow').find('li'),
+```
+
+Sin embargo, esto se puede adaptar fácilmente a las necesidades del programador modificando el valor de la propiedad `carousel.elements`, por ejemplo:
+
+```javascript
+carousel.elements = $(<valor1>).find(<valor2>);
 ```
 
 El código puede copiarse y utilizarse en cualquier sitio puesto que está escrito para ser independiente del resto del que haya en la página. La maquetación también es a cargo del programador.
--- carousel.js
+++ carousel.js
@@ -4,55 +4,97 @@
 * @author Rafael Minguet García
 *
 */
-(function(){
-    var carousel      = $('#slideshow>li');
-    var index         = 0;
-    var timePag       = 3000;
-    var pid;
-    var showNext      = function(){
-        carousel.eq(index).fadeOut(function(){
-            index = ++index < carousel.length ? index : 0;
-            carousel.eq(index).fadeIn();
-        });
-    }
-    var showItem      = function(ii){
-        carousel.eq(index).fadeOut(function(){
-            carousel.eq(ii).fadeIn();
-            index = ii;
-        });
-    }
-    var startCarousel = function(){
-        pid = setInterval(function(){ showNext(); }, timePag);
-    }
-    var stopCarousel  = function(){
-        clearInterval(pid);
-    }
-    
-    // mostrar el primer elemento del carrusel
-    carousel.eq(index).css('display', 'block').fadeIn();
-    // iniciar el carrusel <------------ START!!
-    startCarousel();
-    
-    carousel.hover(stopCarousel, startCarousel);
+var carousel = (function(){
+    var $elements     = $('#slideshow').find('li'),
+        pid           = 0,
+        index         = 0,
+        time          = 3000,
+        isInitialized = false,
 
-    /** MENÚ DE NAVEGACIÓN DEL CARRUSEL */
-    (function(){
-        // Crear el html del menú de navegación
-        var html = '<ul id="nav_carousel">';
-        for (var ii=0; ii<carousel.length; ii++)
-        {
-            html += '<li data-imagen="' + ii + '">' + (ii+1) + '</li>';
-        }
-        html += '</ul>';
-        carousel.parent().append(html);
+        init          = function(){
+            // Mostrar el primer elemento del carrusel
+            elements.eq(index).css('display', 'block').fadeIn();
+            // Si el cursor está encima se detiene el carrusel
+            elements.hover(stopCarousel, startCarousel);
+            isInitialized = true;
+        },
 
-        // Asignar los eventos del menú de navegación
-        var nav = carousel.parent().find('ul#nav_carousel li')
-            .each(function(){
-                $(this).click(function(){
-                    showItem($(this).data('imagen'));
-                })
+        showNext      = function(){
+            elements.eq(index).fadeOut(function(){
+                index = ++index < elements.length ? index : 0;
+                elements.eq(index).fadeIn();
             });
-        nav.hover(stopCarousel, startCarousel);
-    })();
+        },
+
+        showItem      = function(ii){
+            elements.eq(index).fadeOut(function(){
+                elements.eq(ii).fadeIn();
+                index = ii;
+            });
+        },
+
+        startCarousel = function(){
+            if (!isInitialized)
+            {
+                init();
+            }
+            pid = setInterval(function(){ showNext(); }, time);
+        },
+    
+        stopCarousel  = function(){
+            clearInterval(pid);
+        },
+
+        /** Menú de navegación del carrusel */
+        createNav     = function(){
+            // Crear el html del menú de navegación
+            var html = '<ul id="nav_carousel">',
+                maxElements = elements.length;
+            for (var ii=0; ii<maxElements; ii++)
+            {
+                html += '<li data-imagen="' + ii + '">' + (ii+1) + '</li>';
+            }
+            html += '</ul>';
+            var $nav = elements.parent().append(html);
+
+            // Asignar los eventos del menú de navegación
+            $nav = $nav.find('#nav_carousel')
+                .delegate('li', 'click', function(){
+                    $(this).click(function(){
+                        showItem($(this).data('imagen'));
+                    })
+                });
+
+            $nav.find('li').hover(stopCarousel, startCarousel);
+        }
+
+    /** Métodos y propiedades públicas */
+    return {
+        elements      : $elements,
+        startCarousel : startCarousel,
+        stopCarousel  : stopCarousel,
+        createNav     : createNav
+    }
 })();
+
+var controller = {
+    onReady : function(){
+        preparePage();
+
+        carousel.startCarousel();
+        carousel.createNav();
+    }
+};
+
+var preparePage = function(){
+    var $ulSlideshow = $('#slideshow').appendTo('#main');
+    $('#main>:lt(' + --($('#main').children().length) + ')').appendTo('#main');
+    $ulSlideshow.find('li').css(
+        {
+            display : 'none',
+            opacity : 1
+        }
+    );
+};
+
+$(document).ready(controller.onReady);
