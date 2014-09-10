e56d6e2b3c50ab272203a7093fc199cdf74f59b8
Rafael Minguet García <rminguet@gmail.com>
2014-09-10 20:02:09 +0200

    Solución de los warnings que daba el código.

--- carousel.js
+++ carousel.js
@@ -10,29 +10,29 @@ var carousel = (function(){
         index         = 0,
         time          = 3000,
         isInitialized = false,
-
+ 
         init          = function(){
             // Mostrar el primer elemento del carrusel
-            elements.eq(index).css('display', 'block').fadeIn();
+            $elements.eq(index).css('display', 'block').fadeIn();
             // Si el cursor está encima se detiene el carrusel
-            elements.hover(stopCarousel, startCarousel);
+            $elements.hover(stopCarousel, startCarousel);
             isInitialized = true;
         },
-
+ 
         showNext      = function(){
-            elements.eq(index).fadeOut(function(){
-                index = ++index < elements.length ? index : 0;
-                elements.eq(index).fadeIn();
+            $elements.eq(index).fadeOut(function(){
+                index = ++index < $elements.length ? index : 0;
+                $elements.eq(index).fadeIn();
             });
         },
-
+ 
         showItem      = function(ii){
-            elements.eq(index).fadeOut(function(){
-                elements.eq(ii).fadeIn();
+            $elements.eq(index).fadeOut(function(){
+                $elements.eq(ii).fadeIn();
                 index = ii;
             });
         },
-
+ 
         startCarousel = function(){
             if (!isInitialized)
             {
@@ -40,52 +40,52 @@ var carousel = (function(){
             }
             pid = setInterval(function(){ showNext(); }, time);
         },
-    
+   
         stopCarousel  = function(){
             clearInterval(pid);
         },
-
+ 
         /** Menú de navegación del carrusel */
         createNav     = function(){
             // Crear el html del menú de navegación
             var html = '<ul id="nav_carousel">',
-                maxElements = elements.length;
+                maxElements = $elements.length;
             for (var ii=0; ii<maxElements; ii++)
             {
                 html += '<li data-imagen="' + ii + '">' + (ii+1) + '</li>';
             }
             html += '</ul>';
-            var $nav = elements.parent().append(html);
-
+            var $nav = $elements.parent().append(html);
+ 
             // Asignar los eventos del menú de navegación
             $nav = $nav.find('#nav_carousel')
                 .delegate('li', 'click', function(){
                     $(this).click(function(){
                         showItem($(this).data('imagen'));
-                    })
+                    });
                 });
-
+ 
             $nav.find('li').hover(stopCarousel, startCarousel);
-        }
-
+        };
+ 
     /** Métodos y propiedades públicas */
     return {
         elements      : $elements,
         startCarousel : startCarousel,
         stopCarousel  : stopCarousel,
         createNav     : createNav
-    }
+    };
 })();
-
+ 
 var controller = {
     onReady : function(){
         preparePage();
-
+ 
         carousel.startCarousel();
         carousel.createNav();
     }
 };
-
+ 
 var preparePage = function(){
     var $ulSlideshow = $('#slideshow').appendTo('#main');
     $('#main>:lt(' + --($('#main').children().length) + ')').appendTo('#main');
@@ -96,5 +96,5 @@ var preparePage = function(){
         }
     );
 };
-
+ 
 $(document).ready(controller.onReady);
